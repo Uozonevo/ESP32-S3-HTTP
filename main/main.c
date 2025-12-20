419508853d85c1312ae40516bf33bcecebf0f107
Uozonevo <Udokaozonevo3018@gmail.com>
2025-12-20 10:09:04 -0800

    Update code for hosting HTTP Server

--- main/main.c
+++ main/main.c
@@ -4,6 +4,7 @@
 #include <driver/gpio.h>
 #include <sdkconfig.h>
 #include <esp_wifi.h>
+#include <esp_http_server.h> // added for HTTP web server
 #include <string.h>
 #include <esp_err.h>
 #include <esp_log.h>
@@ -56,7 +57,7 @@ void wifi_init_softAP(){
             .password = ESP_WIFI_PASS,
             .channel = ESP_WIFI_CHANNEL,
             .max_connection = MAX_STA_CONN,
-            .authmode = WIFI_AUTH_WAPI_PSK,
+            .authmode = WIFI_AUTH_WPA2_PSK,
             .pmf_cfg = {
                 .required = true,
             },
@@ -72,6 +73,41 @@ void wifi_init_softAP(){
              ESP_WIFI_SSID, ESP_WIFI_PASS, ESP_WIFI_CHANNEL);
 }
 
+// Configure HTTP server 
+httpd_handle_t start_webserver(){
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    // After calling http_start(), server is initialized
+    if (httpd_start(&server, &config) == ESP_OK){
+        ESP_LOGI(TAG, "Server started successfully, registering URI handlers...");
+        return server;
+    }
+    
+    ESP_LOGI(TAG, "Failed to start server");
+    return NULL;
+}
+
+// Request handler function
+esp_err_t my_uri_handler(httpd_req_t* req){
+    const char *resp_str = "<h1>Hello World</h1>";
+    httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+// Configure URI structure defineing the properties being registered
+static const httpd_uri_t http_uri = {
+    .uri = "/",             // Address where source can be found
+    .method = HTTP_GET,     // HTTP method (HTTP_GET, HTTP_POST, ...)
+    .handler = my_uri_handler,        // Function to process request
+    .user_ctx = NULL,       // Additional user data for context
+};
+
 void app_main(void){
     wifi_init_softAP();
+    // Start server calling
+    httpd_handle_t server = start_webserver();
+    // Configure URI management
+    httpd_register_uri_handler(server, &http_uri);
+
 }
